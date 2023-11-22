b9faf8d0b7f098bcdff0d6b7bff760e657361381
Gabriel Meza Prada <standbyg2@gmail.com>
2023-11-22 18:54:28 -0500

    fixed

--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -11,7 +11,7 @@ export class LoginService {
   constructor(private http: HttpClient) { }
 
   login(request: JwtRequest) {
-    return this.http.post("http://localhost:8080/authenticate", request);
+    return this.http.post("https://werecycle-32be86b245c9.herokuapp.com/authenticate", request);
   }
   verificar() {
     let token = sessionStorage.getItem("token");
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment={
     production:false,
-    base:"http://localhost:8080",
+    base:"https://werecycle-32be86b245c9.herokuapp.com",
     apiKey:'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'
 }
 
